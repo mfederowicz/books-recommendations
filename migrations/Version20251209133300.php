0dd944e51c39783783754cc55134b942a6990b73
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-12-09 14:33:47 +0100

    US-005: qdrant client

---
+++ migrations/Version20251209133300.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251209133300 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX idx_tag_recommendation ON recommendations_tags');
+        $this->addSql('DROP INDEX idx_normalized_hash ON recommendations_embeddings');
+        $this->addSql('DROP INDEX idx_name ON tags');
+        $this->addSql('DROP INDEX idx_ascii ON tags');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX idx_normalized_hash ON recommendations_embeddings (normalized_text_hash)');
+        $this->addSql('CREATE INDEX idx_tag_recommendation ON recommendations_tags (tag_id, recommendation_id)');
+        $this->addSql('CREATE INDEX idx_name ON tags (name)');
+        $this->addSql('CREATE INDEX idx_ascii ON tags (ascii)');
+    }
+}
