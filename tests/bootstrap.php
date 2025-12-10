62ca5a48365cda1cfab23f420f90eb5e36dc24fb
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-12-10 23:58:15 +0100

    update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if ((bool) $_SERVER['APP_DEBUG']) {
+if (isset($_SERVER['APP_DEBUG']) && (bool) $_SERVER['APP_DEBUG']) {
     umask(0000);
 }
