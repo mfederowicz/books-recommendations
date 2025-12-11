735ac23185f5f4bf8ddf8316fd99971dd613e8ae
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-12-11 17:55:25 +0100

    debug: auth

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,5 +31,41 @@ final class DefaultController extends AbstractController
     public function wellKnown(): Response
     {
         return new Response('', 404);
+    }
+
+    #[Route('/debug/auth', name: 'debug_auth', methods: ['GET'])]
+    public function debugAuth(Request $request): Response
+    {
+        $user = $this->getUser();
+        $token = $this->container->get('security.token_storage')->getToken();
+        $session = $request->getSession();
+
+        $debug = [
+            'user_from_getUser' => $user ? $user->getEmail() : null,
+            'user_from_app' => $this->get('twig')->getGlobals()['app']->getUser() ? $this->get('twig')->getGlobals()['app']->getUser()->getEmail() : null,
+            'token_exists' => $token !== null,
+            'token_class' => $token ? get_class($token) : null,
+            'token_user' => $token && $token->getUser() ? $token->getUser()->getEmail() : null,
+            'is_authenticated' => $this->isGranted('IS_AUTHENTICATED_FULLY'),
+            'session_id' => $session->getId(),
+            'session_data' => $session->all(),
+            'server_vars' => [
+                'REQUEST_METHOD' => $request->server->get('REQUEST_METHOD'),
+                'HTTP_HOST' => $request->server->get('HTTP_HOST'),
+                'REQUEST_URI' => $request->server->get('REQUEST_URI'),
+                'REMOTE_ADDR' => $request->server->get('REMOTE_ADDR'),
+                'HTTP_USER_AGENT' => $request->server->get('HTTP_USER_AGENT'),
+            ],
+            'cookies' => $request->cookies->all(),
+            'headers' => [
+                'cookie' => $request->headers->get('cookie'),
+                'user-agent' => $request->headers->get('user-agent'),
+                'host' => $request->headers->get('host'),
+            ],
+        ];
+
+        return $this->render('debug/auth.html.twig', [
+            'debug' => $debug,
+        ]);
     }
 }
